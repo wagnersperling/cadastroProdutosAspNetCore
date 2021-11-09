a0210d870f811a6034942e73230ea0a5b3c6afd6
Carlos Wagner <carlos.sperling@granter.com.br>
2021-11-09 13:26:31 -0300

    Customizando Views.

--- src/DevWS.App/ViewModels/ProdutoViewModel.cs
+++ src/DevWS.App/ViewModels/ProdutoViewModel.cs
@@ -23,6 +23,8 @@ namespace DevWS.App.ViewModels
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
+
+        [DisplayName("Imagem do Produto")]
         public IFormFile ImagemUpload { get; set; }
         public string Imagem { get; set; }
 
